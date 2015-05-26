343f567960c4eb75d6808c7c5235ab89de032a18
Anthony Tropeano <tony@swiftax.com>
2015-05-26 12:11:02 -0400

    commit message

--- js/main.js
+++ js/main.js
@@ -1,23 +1,5 @@
 
-//var flowers = ["rose", "lily", "tulip", "lily", "azalea"];
-//flowers.sort();
-//console.log(flowers);
-/*
-for (var i = 1; i <= 100; i++) { 
-    if(i % 3 == 0){ // if value is divisible by the number 2 make it red
-        document.write("<p>Fizz"+"</p>");
-    } else if(i % 5 ==0) {
-    	document.write("<p>Buzz"+"</p>");
-    } else if (i % 3 && i % 5 ==0) {
-    	document.write("<p>fizz buzz"+"</p>");
-    }
 
-    	else {
-        document.write(i + "<br />");
-    }
-}
-
-*/
 
 for (var i = 1; i <= 100; i++) {
 	if (i % 3 == 0 && i % 5 ==0) {
