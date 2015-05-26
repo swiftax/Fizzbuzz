25a3fd1fdf630af50e55003219fefd8efb01ddb1
Anthony Tropeano <tony@swiftax.com>
2015-05-26 11:13:12 -0400

    commit message

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@
 
 for (var i = 1; i <= 100; i++) { 
     if(i % 3 == 0){ // if value is divisible by the number 2 make it red
-        document.write("<p>fizz"+i+"</p><br />");
+        document.write("<p>Fizz"+"</p>");
     } else if(i % 5 ==0) {
-    	document.write("<p>buzz"+i+"</p><br />");
-    } else if (i %3 && i%5==0) {
-    	document.write("<p>fizz buzz"+i+"</p><br />");
+    	document.write("<p>Buzz"+"</p>");
+    } else if (i %3==0 && i % 5==0 ) {
+    	document.write("<p>fizz buzz"+"</p><br />");
     }
 
     	else {
